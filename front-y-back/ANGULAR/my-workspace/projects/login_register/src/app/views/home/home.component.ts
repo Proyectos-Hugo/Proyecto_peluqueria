27deef9490c0d53a8bb118437c4372020bc63825
Proyectos-Hugo <proyectos.hugocambero@gmail.com>
2025-06-19 14:20:40 +0200

    html y css mejorado de santi

--- front-y-back/ANGULAR/my-workspace/projects/login_register/src/app/views/home/home.component.ts
+++ front-y-back/ANGULAR/my-workspace/projects/login_register/src/app/views/home/home.component.ts
@@ -1,8 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [FormsModule,CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
